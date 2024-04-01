803051692a5549f951c0324905d06a5865ff3cbc
tuan.da <doananhtuan22111996@gmail.com>
2024-04-01 17:21:22 +0700

    feat: mocked api by postman, cleanup baseActivity, baseFragment, demo login, paging network loading state, load more state and retry state

--- domain/src/main/java/vn/geekup/app/domain/dto/OTableRequestBody.kt
+++
@@ -1,6 +0,0 @@
-package vn.geekup.app.domain.dto
-
-data class OTableRequestBody(
-    val token: String = "",
-    val currentAuthority: String = ""
-)
--- domain/src/main/java/vn/geekup/app/domain/model/general/RemoteKey.kt
+++
@@ -1,7 +0,0 @@
-package vn.geekup.app.domain.model.general
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-open class RemoteKey(@PrimaryKey val repoId: String, val nextKey: String) : BaseModel()
