d6d990faab933696aee69451bebe4971718698e3
tuan.da <doananhtuan22111996@gmail.com>
2024-04-16 16:09:21 +0700

    feat: OnBoarding - Auto swipe page change

--- app/src/main/java/vn/finance/app/pages/onBoarding/OnBoardingFragment.kt
+++ app/src/main/java/vn/finance/app/pages/onBoarding/OnBoardingFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import vn.finance.app.R
@@ -23,11 +26,13 @@ class OnBoardingFragment :
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentOnboardingBinding
         get() = FragmentOnboardingBinding::inflate
 
-    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: OnBoardingViewPagerAdapter
+    private lateinit var pageJob: Job
 
-    override fun onInit(view: View, savedInstanceState: Bundle?) {
-        viewPager2 = viewBinding.vPager
-        viewPager2.adapter = OnBoardingViewPagerAdapter(
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        adapter = OnBoardingViewPagerAdapter(
             this, items = listOf(
                 OnBoarding(
                     title = getString(R.string.title_1), content = getString(R.string.content_1)
@@ -38,6 +43,39 @@ class OnBoardingFragment :
                 )
             )
         )
-        viewBinding.dotIndicator.attachTo(viewPager2)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onInit(view: View, savedInstanceState: Bundle?) {
+        with(viewBinding.vPager) {
+            adapter = this@OnBoardingFragment.adapter
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    println("PagerView selected: $position")
+                }
+            })
+        }
+        viewBinding.dotIndicator.attachTo(viewBinding.vPager)
+
+        pageJob = lifecycleScope.launch(Dispatchers.Main) {
+            while (true) {
+                println("Pager 2 while: ${viewBinding.vPager.currentItem} --- ${adapter.itemCount}")
+                delay(2000L)
+                if (viewBinding.vPager.currentItem < adapter.itemCount - 1) {
+                    println("Pager 2 changed: ${viewBinding.vPager.currentItem}")
+                    viewBinding.vPager.setCurrentItem(viewBinding.vPager.currentItem + 1, true)
+                } else if (viewBinding.vPager.currentItem == adapter.itemCount - 1) {
+                    println("Pager 2 Reset: ${viewBinding.vPager.currentItem}")
+                    // Reset position = 0
+                    viewBinding.vPager.setCurrentItem(0, true)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        pageJob.cancel()
     }
 }
